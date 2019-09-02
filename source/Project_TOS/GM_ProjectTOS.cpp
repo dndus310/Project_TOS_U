ccb1aca72c7ea19d3473ae643742346bbbf462b0
chowooyeon <dndus310@gmail.com>
2019-09-02 15:14:52 +0900

    aster

---
+++ source/Project_TOS/GM_ProjectTOS.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GM_ProjectTOS.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/Engine.h"
+#include "SwordPlayer.h"
+
+AGM_ProjectTOS::AGM_ProjectTOS()
+{
+
+}
+
+void AGM_ProjectTOS::NextLevel(AActor* actor)
+{
+	FString GetLevelName = GetWorld()->GetMapName();
+	GetLevelName.RemoveFromStart(TEXT("UEDPIE_0_"));
+	GetLevelName = GetLevelName.Reverse();
+	char LevelCountName = GetLevelName[0];
+	int LevelCount = atoi(&LevelCountName);
+	
+	if (LevelCount >= MaxLevelCount)
+	{
+		return;
+	}
+	LevelCount += 1;
+	FString Result = FString::FromInt(LevelCount);
+	GetLevelName[0] = Result[0];
+	GetLevelName = GetLevelName.Reverse();
+
+	UGameplayStatics::OpenLevel(actor,FName(*GetLevelName));
+}
